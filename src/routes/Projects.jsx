b99bc699ef4d47672ed9861205647951b47abca9
Eyüp Özgür <eyupozgurcagliyan@gmail.com>
2024-03-31 03:39:01 +0300

    small fix

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -28,7 +28,7 @@ function Projects() {
 
       <ul className="flex justify-evenly flex-wrap">
               {
-                projects.map((project, key) => <div key={key} className='bg-black w-[500px] min-h-[500px] h-max rounded-lg p-10 max-w-lg text-center h-max border border-white border-opacity-0 mx-16 my-16 flex flex-col justify-start items-center'><img src={project.photo} alt="" className='w-[140px] h-auto mb-5'/> <h1 className='font-bold'>{project.projectName} | id: {key}</h1> <p className='my-[20px]'>{project.description}</p></div>)
+                projects.map((project, key) => <div key={key} className='bg-black w-[500px] min-h-[500px] rounded-lg p-10 max-w-lg text-center h-max border border-white border-opacity-0 mx-16 my-16 flex flex-col justify-start items-center'><img src={project.photo} alt="" className='w-[140px] h-auto mb-5'/> <h1 className='font-bold'>{project.projectName} | id: {key}</h1> <p className='my-[20px]'>{project.description}</p></div>)
               }
       </ul>
 
