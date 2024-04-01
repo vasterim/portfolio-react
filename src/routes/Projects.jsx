688cb5bb04622da3bc84fe1632728da9a5fc6a94
Eyüp Özgür <eyupozgurcagliyan@gmail.com>
2024-04-01 18:32:26 +0300

    fix responsive

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -4,17 +4,6 @@ import profilePicture from '../assets/profile.jpg'
 function Projects() {
 
   let projects = [
-    {projectName: "project", photo: profilePicture, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nobis sunt beatae dolor tenetur est odio libero unde, aspernatur dolorum nihil voluptatibus quisquam quae. Hic sapiente accusamus impedit perspiciatis esse."},
-    {projectName: "project", photo: profilePicture, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nobis sunt beatae dolor tenetur est odio libero unde, aspernatur dolorum nihil voluptatibus quisquam quae. Hic sapiente accusamus impedit perspiciatis esse."},
-    {projectName: "project", photo: profilePicture, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nobis sunt beatae dolor tenetur est odio libero unde, aspernatur dolorum nihil voluptatibus quisquam quae. Hic sapiente accusamus impedit perspiciatis esse."},
-    {projectName: "project", photo: profilePicture, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nobis sunt beatae dolor tenetur est odio libero unde, aspernatur dolorum nihil voluptatibus quisquam quae. Hic sapiente accusamus impedit perspiciatis esse."},
-    {projectName: "project", photo: profilePicture, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nobis sunt beatae dolor tenetur est odio libero unde, aspernatur dolorum nihil voluptatibus quisquam quae. Hic sapiente accusamus impedit perspiciatis esse."},
-    {projectName: "project", photo: profilePicture, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nobis sunt beatae dolor tenetur est odio libero unde, aspernatur dolorum nihil voluptatibus quisquam quae. Hic sapiente accusamus impedit perspiciatis esse."},
-    {projectName: "project", photo: profilePicture, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nobis sunt beatae dolor tenetur est odio libero unde, aspernatur dolorum nihil voluptatibus quisquam quae. Hic sapiente accusamus impedit perspiciatis esse."},
-    {projectName: "project", photo: profilePicture, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nobis sunt beatae dolor tenetur est odio libero unde, aspernatur dolorum nihil voluptatibus quisquam quae. Hic sapiente accusamus impedit perspiciatis esse."},
-    {projectName: "project", photo: profilePicture, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nobis sunt beatae dolor tenetur est odio libero unde, aspernatur dolorum nihil voluptatibus quisquam quae. Hic sapiente accusamus impedit perspiciatis esse."},
-    {projectName: "project", photo: profilePicture, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nobis sunt beatae dolor tenetur est odio libero unde, aspernatur dolorum nihil voluptatibus quisquam quae. Hic sapiente accusamus impedit perspiciatis esse."},
-    {projectName: "project", photo: profilePicture, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nobis sunt beatae dolor tenetur est odio libero unde, aspernatur dolorum nihil voluptatibus quisquam quae. Hic sapiente accusamus impedit perspiciatis esse."},
     {projectName: "project", photo: profilePicture, description: "oh sadly! It's empty here for now!"},
 
   ]
@@ -28,7 +17,7 @@ function Projects() {
 
       <ul className="flex justify-evenly flex-wrap">
               {
-                projects.map((project, key) => <div key={key} className='bg-black w-[500px] min-h-[500px] rounded-lg p-10 max-w-lg text-center h-max border border-white border-opacity-0 mx-16 my-16 flex flex-col justify-start items-center'><img src={project.photo} alt="" className='w-[140px] h-auto mb-5'/> <h1 className='font-bold'>{project.projectName} | id: {key}</h1> <p className='my-[20px]'>{project.description}</p></div>)
+                projects.map((project, key) => <div key={key} className='bg-black w-[500px] max-lg:w-[300px] min-h-[500px] rounded-lg p-10 max-w-lg text-center h-max border border-white border-opacity-0 mx-16 my-16 flex flex-col justify-start items-center'><img src={project.photo} alt="" className='w-[140px] h-auto mb-5'/> <h1 className='font-bold text-2xl'>{project.projectName}</h1> <p className='my-[20px]'>{project.description}</p></div>)
               }
       </ul>
 
